76fdba4ac10146c7ebe041735ea0bb6676b22811
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com.>
2025-06-14 22:10:16 -0400

    Added a callback

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,15 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = Main_Window::new()?;
+
+    main_window.on_install_horde(|| {
+        install_horde();
+    });
+
     main_window.run()
+}
+fn install_python() {} //for now this will do a global install 
+fn install_git() {} //for now this will do a global install 
+fn install_horde() {
+    println!("works here.");
 }
